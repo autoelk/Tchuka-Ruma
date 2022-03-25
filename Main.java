da02a6ad20b6683db12bcd738148f667a04d30dd
Luke Tao <automaticelk@gmail.com>
2022-03-25 09:04:56 -0400

    Change to only output viable states

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-  public static int numBins = 10; // ruma not included
-  public static int numStates = 100000; // number of states to calculate
+  public static int numBins = 20; // ruma not included
+  public static int numStates = 10000; // number of states to calculate
   public static int[][] states = new int[200000][numBins + 1]; // stores all states, first number is last played
 
   public static void main(String[] args) throws IOException {
@@ -15,19 +15,18 @@ class Main {
   }
 
   public static void unsow(int state) {
-    int[] bins = new int[numBins + 1];
-    for (int i = 0; i < bins.length; i++) {
-      bins[i] = states[state][i];
-    }
-    bins[0] = Integer.MAX_VALUE / 2; // place effectively infinite beans in the ruma
-
-    System.out.print("X ");
-    for (int j = 1; j < bins.length; j++) {
-      System.out.print(bins[j] + " ");
-    }
-    System.out.println(" BASE:" + Integer.toBinaryString(state));
+    // for (int i = 0; i < states[state].length; i++) {
+    // System.out.print(states[state][i] + " ");
+    // }
+    // System.out.println(" BASE:" + Integer.toBinaryString(state));
 
     for (int i = 0; i <= 1; i++) {
+      int[] bins = new int[numBins + 1];
+      for (int j = 0; j < bins.length; j++) {
+        bins[j] = states[state][j];
+      }
+      bins[0] = Integer.MAX_VALUE / 2; // place effectively infinite beans in the ruma
+
       // binary translation:
       // 1 = sow from ruma
       // 0 = sow from last sowed position
@@ -37,37 +36,42 @@ class Main {
       }
 
       int beansInHand = 0;
-      while (bins[pos] > 0) {
-        bins[pos]--;
-        pos++;
-        // wrapping
-        // if(pos > numBins) {
-        // pos = 0;
-        // }
-        beansInHand++;
-      }
-      bins[pos] = beansInHand;
-      bins[0] = pos;
-
-      for (int j = 0; j < bins.length; j++) {
-        System.out.print(bins[j] + " ");
-      }
-      System.out.println(" POS: " + i);
-
       int newState = Integer.parseInt(Integer.toBinaryString(state) + i, 2);
-      for (int j = 0; j < bins.length; j++) {
-        states[newState][j] = bins[j];
+      if (newState < numStates && states[state][0] != -1 && bins[pos] > 1) {
+        while (bins[pos] > 0) {
+          bins[pos]--;
+          pos++;
+          // wrapping
+          // if(pos > numBins) {
+          // pos = 0;
+          // }
+          beansInHand++;
+        }
+        bins[pos] = beansInHand;
+        bins[0] = pos;
+
+        // for (int j = 0; j < bins.length; j++) {
+        // System.out.print(bins[j] + " ");
+        // }
+        // System.out.println(" POS: " + i);
+
+        for (int j = 0; j < bins.length; j++) {
+          states[newState][j] = bins[j];
+        }
+      } else {
+        states[newState][0] = -1;
       }
     }
   }
 
   public static void readArray() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader("states.txt"));
-    int index = 0;
     String line;
     while ((line = reader.readLine()) != null) {
-      states[index] = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
-      index++;
+      int[] temp = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+      for (int i = 0; i < temp.length - 1; i++) {
+        states[temp[0]][i] = temp[i + 1]; // first number of input is the index
+      }
     }
     reader.close();
   }
@@ -75,10 +79,13 @@ class Main {
   public static void writeArray() throws IOException {
     FileWriter writer = new FileWriter("states.txt");
     for (int i = 0; i < numStates + 1; i++) {
-      for (int j = 0; j < states[i].length; j++) {
-        writer.write(states[i][j] + " ");
+      if (states[i][0] != -1) {
+        writer.write(i + " "); // output index
+        for (int j = 0; j < states[i].length; j++) {
+          writer.write(states[i][j] + " ");
+        }
+        writer.write("\n");
       }
-      writer.write("\n");
     }
     writer.close();
   }
